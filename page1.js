5c06898e8226f314399aafd33134086f25c59707
abderraouf <abderraoufbessaa@gmail.com>
2023-10-24 10:38:01 +0100

    header section modif

--- page1.js
+++ page1.js
@@ -3,14 +3,23 @@ let searchbar = document.querySelector('.search-bar-container');
 
 window.onscroll = () =>{
    searchbar.classList.remove('active')
+   searchbtn.classList.remove('fa-times')
    menu.classList.remove('fa-times')
     navbar.classList.remove('active')
     loginform.classList.remove('active');
+    if(window.scrollY > 0){
+        document.querySelector('header').classList.add('active');
+    }else{
+        document.querySelector('header').classList.remove('active');
+    }
+    
 }
 
 searchbtn.addEventListener('click', ()=>{
     searchbtn.classList.toggle('fa-times')
     searchbar.classList.toggle('active')
+    document.querySelector('header').classList.add('active');
+    
 });
 
 
@@ -34,6 +43,8 @@ let navbar = document.querySelector('.navbar');
 menu.addEventListener('click', ()=>{
     menu.classList.toggle('fa-times')
     navbar.classList.toggle('active')
+    document.querySelector('header').classList.add('active');
+
 });
 
 
@@ -47,3 +58,5 @@ videobtn.forEach(btn =>{
        document.querySelector('#video-slider').src = src;
     })
 })
+
+
